734334ab682d4ee7ef5329f7e830650b10d8e889
Julia Z <j.zvereva050@gmail.com>
2020-03-03 21:53:01 +0300

    Добавили проверку страницы логина

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,6 +1,7 @@
 from .base_page import BasePage
 from .locators import LoginPageLocators
 
+
 class LoginPage(BasePage):
     def should_be_login_page(self):
         self.should_be_login_url()
@@ -9,15 +10,12 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert "login" in self.browser.current_url.text, "It's not login page URL"
-        assert True
+        assert "login" in self.browser.current_url, "It's not login-page-URL"
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
-        assert self.is_element_present(*MainPageLocators.LOGIN_FORM), "Login form is not presented"
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
 
     def should_be_register_form(self):
         # реализуйте проверку, что есть форма регистрации на странице
-        assert self.is_element_present(*MainPageLocators.REGISTER_FORM), "Register form is not presented"
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Register form is not presented"
