eaef48193c8756022f70c8c8fd86325875f8295b
Julia Z <j.zvereva050@gmail.com>
2020-03-14 12:29:00 +0300

    Добавлен тест на пустую корзину

---
+++ pages/basket_page.py
@@ -0,0 +1,16 @@
+from pages.base_page import BasePage
+from pages.locators import BasketPageLocators
+
+
+class BasketPage(BasePage):
+
+	def basket_should_be_empty(self): # проверяем, что корзина пуста
+		assert self.is_not_element_present(*BasketPageLocators.BASKET_SUMMARY), "Basket is not empty."
+	def basket_should_not_be_empty(self):  # проверяем, что корзина не пуста
+		assert self.is_element_present(*BasketPageLocators.BASKET_SUMMARY), "Basket is empty."
+	def should_be_empty_basket_message(self): # проверяем, что присутствует сообщение о том, что корзина пуста
+		assert "Your basket is empty." in self.browser.find_element(*BasketPageLocators.EMPTY_BASKET_MESSAGE).text,\
+			"There is no message about empty basket."
+	def should_not_be_empty_basket_message(self):  # проверяем, что отсутсвует сообщение о том, что корзина пуста
+		assert "Your basket is empty." not in self.browser.find_element(*BasketPageLocators.EMPTY_BASKET_MESSAGE).text, \
+			"There is a message about empty basket."
